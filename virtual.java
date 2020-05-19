9b70be7927bf9bb8e47f6aedc31c3cce22fc7c7a
alcarras.hector.pulido@gmail.com <alcarras.hector.pulido@gmail.com>
2020-05-19 16:05:28 +0200

    3r commit integrate master

---
+++ virtual.java
@@ -0,0 +1,18 @@
+public class Virtual {
+    private int clons;
+
+    public Virtual(int clons) {
+        this.clons = clons;
+    }
+
+    public Virtual() {
+    }
+
+    public int getClons() {
+        return clons;
+    }
+
+    public void setClons(int clons) {
+        this.clons = clons;
+    }
+}
