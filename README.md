000f0531f87f8559ebf3138f16e46d5c10a32327
alcarras.hector.pulido@gmail.com <alcarras.hector.pulido@gmail.com>
2020-05-19 15:31:49 +0200

    1r comit constructors

---
+++ README.md
@@ -0,0 +1,1 @@
+Primer fitxer al primer repositori de Hector Pulido Tarragona sense coronavirus
---
+++ hectorpulido.java
@@ -0,0 +1,33 @@
+public class NomCognom{
+    private int edat;
+    private String tel;
+
+    NomCognom(int e, String t)
+    {
+        edat=e;
+        tel=t;
+    }
+
+    public NomCognom() {
+    }
+
+    public String toString() {
+        return "NomCognom{" +
+                "edat=" + edat +
+                ", tel='" + tel + '\'' +
+                '}';
+    }
+
+    public boolean equals(Object object) {
+        if (this == object) return true;
+        if (object == null || getClass() != object.getClass()) return false;
+        if (!super.equals(object)) return false;
+
+        NomCognom nomCognom = (NomCognom) object;
+
+        if (edat != nomCognom.edat) return false;
+        if (tel != null ? !tel.equals(nomCognom.tel) : nomCognom.tel != null) return false;
+
+        return true;
+    }
+}
